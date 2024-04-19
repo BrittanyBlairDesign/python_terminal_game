637d61603d5e53e4719d35f5aa1b0e8aab1d7463
Brittany Blair <83046371+BrittanyBlairDesign@users.noreply.github.com>
2024-04-19 15:40:24 -0400

    Updated the vectors.py to include vector2 objects and created some operator functions for addition, subtraction, and equals

--- components.py
+++ components.py
@@ -2,16 +2,43 @@
 # Components in file :
 #     - Movement
 
+import vectors
+
 #### HANDLES THE LOCATION AND MOVEMENT OF CLASSES THAT ARE COMPOSED WITH IT ####
 class Movement:
     def __init__(self):
         pass
     def __repr__(self) -> str:
         pass
-    
-    def GetLocation(self):
+
+#       Return the value of a variable   #
+    def GetLocation(self) -> vectors.Vector2:
         return self.location
-
-
+    def GetDirection(self) -> vectors.Vector2:
+        return self.direction
+    def GetPossibleDirections(self) -> str[str]:
+        return self.directions.keys()
+    
+#       Set the value of the a Variable  #
+    def SetLocation(self, newLocation: vectors.Vector2) -> None:
+        self.location = newLocation
+    def SetLocation(self, x: float| int , y: float|int) -> None:
+        self.location.x = x
+        self.location.y = y
+    def setDirection(self, direction:str):
+        if direction in self.directions.keys():
+            new_direction = vectors.Vector2(self.directions[direction][0], self.directions[direction][1])
+    
+#       Game related Methods             #
+    def Move(self) -> vectors.Vector2:
+        pass
+    def isMoveValid(self):
+        pass
+    
     ## Movement variables ##
-    location = Vector2()
+    location: vectors.Vector2 = vectors.Vector2()
+    direction: vectors.Vector2 = vectors.Vector2(0)
+    directions = {"up":   [-1, 0],
+                  "down": [1, 0],
+                  "right":[0, -1],
+                  "left": [0, 1]}
--- vectors.py
+++ vectors.py
@@ -2,18 +2,40 @@
 # Current Vector classes
 #   - Vector2()
 
-type Vector2 = Vector2
-def __add__(Vector2_1: Vector2, Vector2_2: Vector2 ):
-    pass
+# Type alias for Vector2 class
+type Vector2  = Vector2
+
+# Operator overloads for Vector2 class
+#   Adding 2 vectors    #
+def __add__(vector1: Vector2, vector2: Vector2 ):
+    return Vector2(vector1.x + vector2.x, vector1.y + vector2.y )
+
+#   Adding a float or int to a vector    #
+def __add__(vector1: Vector2, x: float|int):
+    return Vector2(vector1.x + x , vector1.y + x)
+
+#   Subtracting 2 vectors   #
+def __sub__(vector1: Vector2, vector2: Vector2 ):
+    return Vector2(vector1.x - vector2.x, vector1.y - vector2.y)
+
+#   Subtracting a float or int from a vector    #
+def __sub__(vector1: Vector2, x: float|int):
+    return Vector2(vector1.x - x , vector1.y - x)
+
+#   Setting one vector equal to another   #
+def __eq__(vector1: Vector2, vector2: Vector2):
+    vector1.x = vector2.x
+    vector1.y = vector2.y
+    return vector1
 
 ### CLASS FOR VECTORS WITH 2 VALUES ###
 class Vector2:
     def __init__(self) -> None:
         pass
-    def __init__(self, x):
+    def __init__(self, x: float| int):
         self.x = x
         self.y = x
-    def __init__(self, x, y):
+    def __init__(self, x: float| int, y: float | int):
         self.x = x
         self.y = y
 
@@ -21,5 +43,5 @@ class Vector2:
         pass
 
     ## Vector2 Variables ##
-    x = 0
-    y = 0
+    x: float| int = 0
+    y: float| int = 0
