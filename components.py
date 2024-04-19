d1790b26306e1e487d2a79116c40cda7e6b06be3
Brittany Blair <83046371+BrittanyBlairDesign@users.noreply.github.com>
2024-04-19 16:37:09 -0400

    edits

--- components.py
+++ components.py
@@ -1,22 +1,24 @@
 # File contains components for various classes to use in the game. 
 # Components in file :
 #     - Movement
+#     - Stat
 
 import vectors
+import random
 
 #### HANDLES THE LOCATION AND MOVEMENT OF CLASSES THAT ARE COMPOSED WITH IT ####
 class Movement:
     def __init__(self):
         pass
     def __repr__(self) -> str:
-        pass
+        return "Location :" + self.location + "\nDirection : " + self.direction
 
 #       Return the value of a variable   #
     def GetLocation(self) -> vectors.Vector2:
         return self.location
     def GetDirection(self) -> vectors.Vector2:
         return self.direction
-    def GetPossibleDirections(self) -> str[str]:
+    def GetPossibleDirections(self) -> list[str]:
         return self.directions.keys()
     
 #       Set the value of the a Variable  #
@@ -28,17 +30,94 @@ class Movement:
     def setDirection(self, direction:str):
         if direction in self.directions.keys():
             new_direction = vectors.Vector2(self.directions[direction][0], self.directions[direction][1])
+        else:
+            UserWarning("{d} is not a valid direction to move in")
     
 #       Game related Methods             #
     def Move(self) -> vectors.Vector2:
-        pass
-    def isMoveValid(self):
-        pass
+        self.location += self.direction
+        return self.location
+    def MoveRandom(self) -> vectors.Vector2:
+        self.Location += random.choice(self.directions.keys())
+    def TestSurroundings(self) -> list[vectors.Vector2]:
+        surroundings = []
+        for d in self.directions.values():
+            surroundings.append(self.direction + vectors.Vector2(d[0],d[y]))
+        return surroundings
     
     ## Movement variables ##
-    location: vectors.Vector2 = vectors.Vector2()
-    direction: vectors.Vector2 = vectors.Vector2(0)
-    directions = {"up":   [-1, 0],
+    location: vectors.Vector2 = vectors.Vector2(0,0)
+    direction: vectors.Vector2 = vectors.Vector2(0,0)
+    directions: dict[str, list[int]] = {"up":   [-1, 0],
                   "down": [1, 0],
                   "right":[0, -1],
                   "left": [0, 1]}
+    
+#### HANDLES THE  IFORMATION REGARDING A SPECIFIC STAT OF AN OBJECT ####
+class Stat:
+    def __init__(self):
+        self.value = 1
+        self.max_value = 1
+        self.min_value = 0
+    def __init__(self, value: int | float):
+        self.value = value
+        self.max_value = value
+        self.min_value = value
+    def __init__(self, min_value: int | float, max_value: int | float):
+        self.value = max_value
+        self.max_value = max_value
+        self.min_value = min_value
+    def __repr__(self) -> str:
+        return "{x}/{y}".format(x=str(self.value),y=str(self.max_value))
+#       Get the value of current stat variables   #
+    def GetValue(self) -> int | float:
+        return self.value
+    def GetMax(self) -> int | float:
+        return self.max_value
+    def GetMin(self) -> int | float:
+        return self.min_value
+    
+#       Make changes to the 'value' variable      #
+    def Increase(self, x: int | float):
+        if self.value + x <= self.max_value:
+            self.value += x
+        else:
+            self.value = self.max_value
+    def Decrease(self, x: int | float):
+        if self.value - x > self.min_value:
+            self.value -= x
+        else:
+            self.value = self.min_value
+    def Reset(self):
+        self.value = max
+
+    ## Stat variables ##
+    value: int | float
+    min_value: int | float
+    max_value: int | float
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- vectors.py
+++ vectors.py
@@ -5,42 +5,44 @@
 # Type alias for Vector2 class
 type Vector2  = Vector2
 
-# Operator overloads for Vector2 class
-#   Adding 2 vectors    #
-def __add__(vector1: Vector2, vector2: Vector2 ):
-    return Vector2(vector1.x + vector2.x, vector1.y + vector2.y )
-
-#   Adding a float or int to a vector    #
-def __add__(vector1: Vector2, x: float|int):
-    return Vector2(vector1.x + x , vector1.y + x)
-
-#   Subtracting 2 vectors   #
-def __sub__(vector1: Vector2, vector2: Vector2 ):
-    return Vector2(vector1.x - vector2.x, vector1.y - vector2.y)
-
-#   Subtracting a float or int from a vector    #
-def __sub__(vector1: Vector2, x: float|int):
-    return Vector2(vector1.x - x , vector1.y - x)
-
-#   Setting one vector equal to another   #
-def __eq__(vector1: Vector2, vector2: Vector2):
-    vector1.x = vector2.x
-    vector1.y = vector2.y
-    return vector1
-
 ### CLASS FOR VECTORS WITH 2 VALUES ###
 class Vector2:
-    def __init__(self) -> None:
-        pass
-    def __init__(self, x: float| int):
-        self.x = x
-        self.y = x
-    def __init__(self, x: float| int, y: float | int):
+    def __init__(self, x: float| int = 0, y: float | int = 0) -> None:
         self.x = x
         self.y = y
 
+    # Operator overloads for Vector2 class  #
+    #   Adding 2 vectors    #
+    def __add__(self, vector2: Vector2 ) -> Vector2:
+        return Vector2(self.x + vector2.x, self.y + vector2.y )
+
+    #   Adding a float or int to a vector    #
+    def __add__(self, x: float|int) -> Vector2:
+        return Vector2(self.x + x , self.y + x)
+
+    #   Subtracting 2 vectors   #
+    def __sub__(self, vector2: Vector2 ) -> Vector2:
+        return Vector2(self.x - vector2.x, self.y - vector2.y)
+
+    #   Subtracting a float or int from a vector    #
+    def __sub__(self, x: float|int) -> Vector2:
+        return Vector2(self.x - x , self.y - x)
+
+    #   Setting one vector equal to another   #
+    def __eq__(self, vector2: Vector2) -> Vector2:
+        self.x = vector2.x
+        self.y = vector2.y
+        return self
+
+    #   Setting one vector to += another    #
+    def __iadd__(self, vector2: Vector2) -> Vector2:
+        self.x += vector2.x
+        self.y += vector2.y
+        return self
+    
+    #   Prints out (x,y) when printing a vector #
     def __repr__(self) -> str:
-        pass
+        return "({x},{y})".format(x=str(x),y=str(y))
 
     ## Vector2 Variables ##
     x: float| int = 0
